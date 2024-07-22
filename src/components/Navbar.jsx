660015fcf09541e190c375d70482df564c05fc80
Prakriti Adhikari <prakritiadhikari2121@gmail.com>
2024-07-22 20:24:43 +0545

    NavBar Added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import Layout from '../layouts/Layout.astro'; 
+import companyLogo from '../assets/pagelogo.png'; 
+
+const Navbar = () => {
+  return (
+    <nav className='relative container mx-auto p-6'>
+      <div className='flex items-center justify-between'>
+        <div className='pt-2'>
+        <img src='../src/assets/pagelogo.png' alt='Company Logo' className='w-21 h-auto' />
+        </div>
+        <div className='hidden space-x-6 md:flex'>
+          <a href='#' className='hover:text-darkGrayishBlue'>
+            Pricing
+          </a>
+          <a href='#' className='hover:text-darkGrayishBlue'>
+            Product
+          </a>
+          <a href='#' className='hover:text-darkGrayishBlue'>
+            About Us
+          </a>
+          <a href='#' className='hover:text-darkGrayishBlue'>
+            Careers
+          </a>
+          <a href='#' className='hover:text-darkGrayishBlue'>
+            Community
+          </a>
+        </div>
+        <a
+          href='#'
+          className='hidden p-3 px-6 pt-2 text-white bg-brightRed rounded-full baseline hover:bg-brightRedLight md:block'
+        >
+          Get Started
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
