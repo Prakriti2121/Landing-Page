2e4cc0385924c4f7e8c6090f3266122eb93cb4cd
Prakriti Adhikari <prakritiadhikari2121@gmail.com>
2024-07-30 22:20:47 +0545

    Added reusable Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from "react";
+import { cva } from "class-variance-authority";
+import PropTypes from "prop-types";
 
-const Button = () => {
+// Define the button variants using cva
+const buttonVariants = cva(
+  "p-3 px-6 pt-2 text-white bg-brightRed rounded-full  hover:bg-brightRedLight",
+  {
+    variants: {
+      variant: {
+        primary: "bg-blue-500 text-white",
+        secondary: "bg-gray-500 text-white",
+      },
+    },
+  }
+);
+
+// Define the Button component
+const Button = ({ variant, children, ...props }) => {
   return (
-    <div className='p-3 px-6 pt-2 text-white bg-brightRed rounded-full  hover:bg-brightRedLight"'>
-      Get Started
-    </div>
+    <button className={buttonVariants({ variant })} {...props}>
+      {children}
+    </button>
   );
 };
 
+// Define the proptypes for the Button component
+Button.propTypes = {
+  variant: PropTypes.oneOf(["primary", "secondary"]).isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+// Define the default proptypes for the Button component
+Button.defaultProps = {
+  variant: "primary",
+};
 export default Button;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,33 @@
-import React from 'react'; 
-import companyLogo from '../assets/pagelogo.png'; 
+import React from "react";
+import companyLogo from "../assets/pagelogo.png";
 // import '../Styles/global.css';
-import Button from '../components/Button';
-
+import Button from "../components/Button";
 
 const Navbar = () => {
   return (
-    <nav className='container mx-auto p-6'>
-      <div className='flex items-center justify-between'>
-        <div className='pt-2'>
-          <img src='../src/assets/pagelogo.png' alt='Company Logo' className='w-21 h-auto' />
+    <nav className="container mx-auto p-6">
+      <div className="flex items-center justify-between">
+        <div className="pt-2">
+          <img
+            src="../src/assets/pagelogo.png"
+            alt="Company Logo"
+            className="w-21 h-auto"
+          />
         </div>
-        <div className='hidden space-x-6 md:flex'>
-          <a href='#'>Pricing</a>
-          <a href='#'>Product</a>
-          <a href='#'>About Us</a>
-          <a href='#'>Careers</a>
-          <a href='#'>Community</a>
+        <div className="hidden space-x-6 md:flex">
+          <a href="#">Pricing</a>
+          <a href="#">Product</a>
+          <a href="#">About Us</a>
+          <a href="#">Careers</a>
+          <a href="#">Community</a>
         </div>
-        <a href='#' className='hidden md:block'>
-         <Button />
+        <a href="#" className="hidden md:block">
+          <Button
+            variant="primary"
+            onClick={() => alert("Primary Button Clicked!")}
+          >
+            Primary Button
+          </Button>
         </a>
       </div>
     </nav>
